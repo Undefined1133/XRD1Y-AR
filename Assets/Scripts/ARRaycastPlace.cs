ba888d4c105b1d6452a5ca8f1239a74f221d548a
Dmitrii Pinazri <coolnick0111@gmail.com>
2023-10-02 19:26:27 +0200

    Initial commit

---
+++ Assets/Scripts/ARRaycastPlace.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public ARRaycastManager raycastManager;
+
+    public GameObject objectToInstantiate;
+    public GameObject instantiatedGameObject;
+
+    public Camera camera;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            if (raycastManager.Raycast(ray, hits, TrackableType.Planes))
+            {
+                Pose pose = hits[0].pose;
+                if (instantiatedGameObject == null)
+                {
+                    instantiatedGameObject = Instantiate(objectToInstantiate, pose.position, pose.rotation);
+                }
+                instantiatedGameObject.transform.position = pose.position;
+            }
+        }
+    }
+}
